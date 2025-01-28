46530365f3e9e981c3f6400d5568b48263662bdc
Amal Benny <57700315+amalbenny7@users.noreply.github.com>
2025-01-28 15:13:41 +0000

    candidate card full responsive -acordion expanson to be fixed

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const accordions = document.querySelectorAll(".accordion");
-
-    accordions.forEach((accordion) => {
-        accordion.addEventListener("click", function () {
-            // Toggle active class for the clicked beige accordion
-            this.classList.toggle("active");
-
-            // Toggle the associated panel
-            const panel = this.nextElementSibling;
-            if (panel.classList.contains("show")) {
-                panel.classList.remove("show");
-            } else {
-                panel.classList.add("show");
-            }
-        });
-    });
-});
+//test script
