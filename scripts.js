bf10cdd30aa4d8206044d942699b335364f4c331
Amal Benny <57700315+amalbenny7@users.noreply.github.com>
2025-01-31 10:57:43 +0000

    accordions with 5

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-//test script
+document.addEventListener("DOMContentLoaded", function () {
+    const accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach(accordion => {
+        accordion.addEventListener("click", function () {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    });
+});
